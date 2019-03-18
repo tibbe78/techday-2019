67dc67a50ec369a665cfa6cf0c67a7658afe5a02
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2019-03-18 10:38:42 +0100

    demo5 fixes and patches

--- demo5/README.md
+++ demo5/README.md
@@ -7,26 +7,52 @@ Ansible automation demo for Cygate Techdays 2019 by Christofer Tibbelin
 
 Using CheckPoints Ansible module https://github.com/CheckPointSW/cpAnsible
 
+# Create docker group and add user to this group. ReInit group.
+> Now we don't need to sudo for docker
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+newgrp docker
+
+#### install docker python on your docker host.
+> so ansible can talk to this.
+```sh
+pip install docker
+```
+
 #### install the [CheckPoint MGT API](https://github.com/CheckPointSW/cp_mgmt_api_python_sdk) in a Docker container via the Dockerfile
 > This is what Ansible needs to talk to.
 ```sh
 apt install -y python-pip
 pip install -y git+https://github.com/CheckPointSW/cp_mgmt_api_python_sdk
 ```
-#### Generate keys for your docker container
+
+#### Generate new keys for your docker container
 ```sh
 /usr/bin/ssh-keygen -q -t rsa -N '' -f ./docker/ssh_keys/id_rsa
 ```
 
+#### Create docker network
+docker network list | grep -q "mgt_net" || docker network create "mgt_net"
+
 #### Build docker file
 ```sh
-sudo docker build --tag=sshd_cp_mgt_api ./docker/
+docker build --tag=cp_mgt_api ./docker/
 ```
 
 #### Start the Docker container
 ```sh
-sudo docker run -d -P -v "$(pwd)"/docker/ssh_keys:/home/cygate/.ssh --name cp_api_sshd1 sshd_cp_mgt_api
+docker run -d -P \
+  --network='mgt_net' \
+  --network-alias mgt \
+  -v "$(pwd)"/docker/ssh_keys:/home/cygate/.ssh \
+  --name cp_mgt_api01 cp_mgt_api
 ```
+
+#### check if it's runnin
+docker ps
+
+#### logon to the container with shell
+docker exec -it cp_mgt_api01 bash
 
 ### [Demo 6](../demo6/) :dog:
 Next Demo 6 (Secure our sudo password with Ansible Vault)
--- demo5/demo5.sh
+++ demo5/demo5.sh
@@ -1,27 +1,33 @@
 #!/bin/sh
 
-echo "stop and remove container if running"
-sudo docker container stop cp_api_sshd1
-sudo docker container rm cp_api_sshd1
-sleep 5
+# stop and remove container if running
+docker container stop checkpoint_api_01
+docker container rm checkpoint_api_01
 
-echo "remove old ssh_keys"
-rm ./docker/ssh_keys/id_rsa
-rm ./docker/ssh_keys/id_rsa.pub
-sleep 5
+# remove old ssh_keys
+#rm ./docker/ssh_keys/id_rsa
+#rm ./docker/ssh_keys/id_rsa.pub
 
-echo "Genereate keys for the docker containers to use."
-/usr/bin/ssh-keygen -q -t rsa -N '' -f ./docker/ssh_keys/id_rsa
-sleep 5
+# Genereate keys for the docker containers to use if they don't exist
+if [ ! -f ./docker/ssh_keys/id_rsa ]; then
+    /usr/bin/ssh-keygen -q -t rsa -N '' -f ./docker/ssh_keys/id_rsa
+fi
 
-echo "Build the image from the Dockerfile"
-sudo docker build --tag=sshd_cp_mgt_api ./docker/
-sleep 5
+# Build the image from the Dockerfile
+docker build --tag=checkpoint_api ./docker/
 
-echo "Create the docker container from the image."
-echo "Also send in the local ssh_keys as the container .ssh folder"
-sudo docker run -d -P -v "$(pwd)"/docker/ssh_keys:/home/cygate/.ssh --name cp_api_sshd1 sshd_cp_mgt_api
-sleep 5
+# Create the Management network in docker.
+docker network list | grep -q "mgt_net" || docker network create "mgt_net"
 
-echo "Check the container is running again"
-sudo docker ps
+
+# Create the docker container from the image.
+# Also send in the local ssh_keys as the container .ssh folder
+docker run -d -P \
+  --network='mgt_net' \
+  --network-alias mgt \
+  -v "$(pwd)"/docker/ssh_keys:/home/cygate/.ssh \
+  --name checkpoint_api_01 checkpoint_api
+sleep 1
+
+#Check the container is running again
+docker ps

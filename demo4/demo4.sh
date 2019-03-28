b3912eaae21de02a8fd61ba0b80c3fa1ad3ccb49
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2019-03-28 09:15:02 +0100

    demo4.sh script

---
+++ demo4/demo4.sh
@@ -0,0 +1,16 @@
+# Create a random common password in your home folder.
+tr -cd '[:alnum:]' < /dev/urandom | fold -w32 | head -n1 > ~/.vault-pass.common
+# check that it looks ok
+cat ~/.vault-pass.common
+# edit ansible.cfg and add:
+# [defaults]
+# vault_identity_list = common@~/.vault-pass.common
+sudo nano /etc/ansible/ansible.cfg
+# edit the common vault (the file containing your secret passwords or varibles)
+nano vault_common.yml
+# Encrypt the vault_common.yml with the common password
+ansible-vault encrypt --encrypt-vault-id common vault_common.yml
+# Check that it's encrypted
+cat vault_common.yml
+# Run the playbook without need of sudo.
+ansible-playbook -i inventory.ini playbook.yml

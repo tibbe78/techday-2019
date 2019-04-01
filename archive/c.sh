c607d61afe58247e5a5fa555b3e10dd6061a39be
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2019-04-01 10:23:01 +0200

    start demo6

--- archive/c.sh
+++ archive/c.sh
@@ -1,36 +1,45 @@
 #!/bin/bash
 
+# Colors for fake command prompt
+# Green
 grn=$'\e[1;32m'
+# Blue
 blu=$'\e[1;34m'
+# Guess
 blink=$'\e[1;5m'
+# End of colors
 end=$'\e[0m'
+# Store the commands file in the commands varible
+commands=`cat commands.sh`
+# command varible is each command we fake type in.
+command=''
 
-foo=`cat commands.sh`
-comm=''
-
+# Our local path
 pwd=`pwd`
 
+# Print a fake command prompt with proper colors.
 printf "${grn}cygate@ubuntuV1${end}:${blu}~${pwd#*/home/cygate}${end}\$ "
 
-for (( i=0; i<${#foo}; i++ )); do
+
+for (( i=0; i<${#commands}; i++ )); do
   read -rsn1 input
-  if [ "${foo:$i:1}" = $'\n' ]; then
+  if [ "${commands:$i:1}" = $'\n' ]; then
     while [[ $input != "" ]]; do
       read -rsn1 input
     done
-    comm+=$'\n'
+    command+=$'\n'
     printf $'\n'
-    eval $comm
-    comm=''
+    eval $command
+    command=''
 printf "${grn}cygate@ubuntuV1${end}:${blu}~${pwd#*/home/cygate}${end}\$ "
   else
-    comm+="${foo:$i:1}"
-    printf "${foo:$i:1}"
+    command+="${commands:$i:1}"
+    printf "${commands:$i:1}"
   fi
 done
 while [[ $input != "" ]]; do
   read -rsn1 input
 done
 printf $'\n'
-comm+=$'\n'
-eval $comm
+command+=$'\n'
+eval $command
---
+++ demo6/README.md
@@ -0,0 +1,10 @@
+### Cygate Techdays 2019
+Ansible automation demo for Cygate Techdays 2019 by Christofer Tibbelin
+
+## Ansible Demo 6 :blue_book::green_book::orange_book:
+
+### How Ansible roles can simplify playbook complexity
+
+
+### [Demo 7](demo7/) :runner::books::grin:
+Use RunDeck to bind it all together.

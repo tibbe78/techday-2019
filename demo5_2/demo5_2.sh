8dd3621d561d2d51eed2bbbae6e7320d7370f311
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2019-04-06 13:15:28 +0200

    added more test

--- demo5_2/demo5_2.sh
+++ demo5_2/demo5_2.sh
@@ -1,13 +1,19 @@
 #!/bin/sh
 
-# Check the container is running still
-docker ps
-
 # clone the module from git
 git clone https://github.com/CheckPointSW/cpAnsible
 
 # copy the actual ansible module catalog to the default ansible custom modules place.
 cp -r ./cpAnsible/check_point_mgmt /usr/share/my_modules
 
-# Run the playbook to add object to firewall
+# Check the container is running still
+docker ps
+
+# Run the playbook to check if it would have worked to add objects to firewall
+ansible-playbook -C -i inventory.ini add_fw_obj.yml
+
+# Run the playbook to add objects to firewall
 ansible-playbook -i inventory.ini add_fw_obj.yml
+
+# Run the playbook to remove the objects to firewall
+ansible-playbook -i inventory.ini del_fw_obj.yml

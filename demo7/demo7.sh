6735abecc0b5c96b489ab00cefe5f0f88d144377
Christofer Tibbelin <christofer.tibbelin@cygate.se>
2019-04-13 16:41:40 +0200

    more info

--- demo7/demo7.sh
+++ demo7/demo7.sh
@@ -3,6 +3,7 @@
 # Rundeck works like ansible using SSH and also using ansible localy
 # So it have to be installed on the same machine as ansible.
 # There is a ansible plugin for rundeck that makes it easy to work with ansible.
+# Might be possible to install rundeck in a container. but don't know how...
 
 mkdir downloads
 
@@ -24,14 +25,19 @@ sudo apt-get install rundeck
 # Download the rundeck ansible plugin and put it in the default pluginfolder.
 wget -P /var/lib/rundeck/libext https://github.com/Batix/rundeck-ansible-plugin/releases/download/3.0.1/ansible-plugin-3.0.1.jar
 
-#change owner of plugin. might not be needed.
+# change owner of plugin. might not be needed.
+# rundeck just really nead read access
 sudo chown rundeck:rundeck /var/lib/rundeck/libext/ansible-plugin-3.0.1.jar
 
-#remove old ansible plugin included in release
+# remove old ansible plugin included in release
+# for me it was version 2.5
 sudo rm /var/lib/rundeck/libext/rundeck-ansible*
 
-# Probably need to fix folder rights for ansible to write to the rundeck catalog.
+# Need to fix folder rights for ansible to write to the rundeck catalog.
 sudo chown rundeck:rundeck /var/lib/rundeck
+
+# same with the /etc/rundeck as it's used for ACL lists.
+sudo chown rundeck:rundeck /etc/rundeck
 
 # Add your user to rundeck group if you want to share your files with rundeck.
 sudo usermod -a -G rundeck cygate
@@ -39,8 +45,10 @@ sudo usermod -a -G rundeck cygate
 sudo usermod -a -G cygate rundeck
 
 #Test that ansible works
+# This should not give any java errors or such.
 sudo su rundeck -s /bin/bash -c "ansible all -m ping"
 
+# enable and restart rundeck daemon.
 sudo systemctl enable rundeckd
 sudo systemctl restart rundeckd
 
